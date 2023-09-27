9a20e30455c2dedc3f8ef363966d4fba7c848a8b
GeoAvi <georgenavendalkaran@gmail.com>
2023-09-28 01:18:23 +0530

    adding server

--- src/App.js
+++ src/App.js
@@ -5,21 +5,59 @@ import TodoForm from '../src/Components/TodoForm.js';
 import TodoItem from '../src/Components/TodoItems';
 
 function App() {
-  const [uncompletedTasks, setUncompletedTasks] = useState([]);
-  const [completedTasks, setCompletedTasks] = useState([]);
+  // two const created as uncompleted and completed task to take care of the sequence in which the task appear in completed and uncompleted list
+  const [uncompletedTasks, setUncompletedTasks] = useState(() => {
+    const savedUncompletedTasks =
+      JSON.parse(localStorage.getItem('uncompletedTasks')) || [];
+    return savedUncompletedTasks;
+  });
+  const [completedTasks, setCompletedTasks] = useState(() => {
+    const savedCompletedTasks =
+      JSON.parse(localStorage.getItem('completedTasks')) || [];
+    return savedCompletedTasks;
+  });
+
   const [taskIdCounter, setTaskIdCounter] = useState(1);
 
+  useEffect(() => {
+    // Load task data from localStorage on component mount
+    const savedUncompletedTasks = JSON.parse(
+      localStorage.getItem('uncompletedTasks')
+    );
+    const savedCompletedTasks = JSON.parse(
+      localStorage.getItem('completedTasks')
+    );
+
+    if (savedUncompletedTasks) {
+      setUncompletedTasks(savedUncompletedTasks);
+    }
+
+    if (savedCompletedTasks) {
+      setCompletedTasks(savedCompletedTasks);
+    }
+    console.log('localStorage updated');
+  }, []); // Run this effect only once on component mount
+
+  useEffect(() => {
+    // Save task data to localStorage whenever tasks change
+    localStorage.setItem('uncompletedTasks', JSON.stringify(uncompletedTasks));
+    localStorage.setItem('completedTasks', JSON.stringify(completedTasks));
+    console.log('localStorage on updated');
+  }, [uncompletedTasks, completedTasks]);
+
+  //   function to handle adding of new task
   const addTask = (text) => {
     const newTask = {
       id: taskIdCounter,
       text: text,
       completed: false,
     };
-
+    // sequencing the task with newly added task first followed by previous uncompleted task
     setUncompletedTasks([newTask, ...uncompletedTasks]);
     setTaskIdCounter(taskIdCounter + 1);
   };
 
+  //function to identify completed tasks
   const completeTask = (taskId) => {
     // Find the task to complete in uncompleted tasks
     const taskToComplete = uncompletedTasks.find((task) => task.id === taskId);
@@ -39,34 +77,11 @@ function App() {
     }
   };
 
+  //function to reset the list to blank on clicking of reset button
   const resetList = () => {
     setCompletedTasks([]);
     setUncompletedTasks([]);
   };
-
-  useEffect(() => {
-    // Load task data from localStorage on component mount
-    const savedUncompletedTasks = JSON.parse(
-      localStorage.getItem('uncompletedTasks')
-    );
-    const savedCompletedTasks = JSON.parse(
-      localStorage.getItem('completedTasks')
-    );
-
-    if (savedUncompletedTasks) {
-      setUncompletedTasks(savedUncompletedTasks);
-    }
-
-    if (savedCompletedTasks) {
-      setCompletedTasks(savedCompletedTasks);
-    }
-  }, []); // Run this effect only once on component mount
-
-  useEffect(() => {
-    // Save task data to localStorage whenever tasks change
-    localStorage.setItem('uncompletedTasks', JSON.stringify(uncompletedTasks));
-    localStorage.setItem('completedTasks', JSON.stringify(completedTasks));
-  }, [uncompletedTasks, completedTasks]);
 
   return (
     <div>
